20f0046b27e07436d73236b079763ee5845bf801
Christian Everett <ceverett@gmail.com>
2016-02-04 16:28:36 -0800

    fixed form clearing on nav from success page

--- app/routes/success.js
+++ app/routes/success.js
@@ -3,11 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     var pmt = this.controllerFor('send');
+    var payee = pmt.get('payee');
+    var amount = pmt.get('amount');
+    pmt.set('payee', '');
+    pmt.set('amount', '');
+    pmt.set('message', '');
     return new Ember.RSVP.Promise(function(resolve) {
       Ember.run.later(function() {
         resolve({
-          payee: pmt.get('payee'),
-          amount: pmt.get('amount')
+          payee: payee,
+          amount: amount
         });
       }, 3000);
     });
